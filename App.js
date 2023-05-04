424de36b447258b6a782e4e99c7da54b8a1369c9
jonathan_baraza <jbaraza46@gmail.com>
2023-05-04 22:45:02 +0300

    loader state

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { ActivityIndicator, View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
   return (
     <NavigationContainer>
-      <Tabs />
+      {loading ? (
+        <View style={styles.container}>
+          <ActivityIndicator size={"large"} color={"#007acc"} />
+        </View>
+      ) : (
+        <Tabs />
+      )}
     </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+});
 
 export default App;
