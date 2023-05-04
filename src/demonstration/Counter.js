f649365e4531f0038d920c38e91eb2610a1a1491
jonathan_baraza <jbaraza46@gmail.com>
2023-05-04 20:56:37 +0300

    counter input feat

--- src/demonstration/Counter.js
+++ src/demonstration/Counter.js
@@ -5,10 +5,12 @@ import {
   StyleSheet,
   TouchableOpacity,
   TouchableHighlight,
+  TextInput,
 } from "react-native";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+  const [countInput, setCountInput] = useState(0);
   const handleCount = (isAdding) => {
     setCount(isAdding ? count + 1 : count - 1);
   };
@@ -32,11 +34,27 @@ const Counter = () => {
           onPress={() => {
             handleCount(false);
           }}
-          title="+1"
           style={styles.btn}
           accessibilityLabel="Learn more about this purple button"
         >
           <Text style={styles.btnText}>-1</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter Count"
+          onChangeText={(newcount) => setCountInput(newcount)}
+          value={countInput}
+        />
+        <TouchableOpacity
+          onPress={() => {
+            setCount(countInput);
+          }}
+          style={styles.btnTwo}
+          accessibilityLabel="Learn more about this purple button"
+        >
+          <Text style={styles.btnText}>ADD</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -63,6 +81,14 @@ const styles = StyleSheet.create({
     padding: 10,
     alignItems: "center",
   },
+  btnTwo: {
+    backgroundColor: "#007acc",
+    flex: 1,
+    margin: 10,
+    width: 100,
+    padding: 10,
+    alignItems: "center",
+  },
   btnText: {
     color: "white",
     fontSize: 15,
@@ -72,6 +98,13 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     padding: 10,
     alignItems: "flex-start",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "gray",
+    padding: 10,
+    borderRadius: 5,
+    margin: 20,
   },
 });
 export default Counter;
