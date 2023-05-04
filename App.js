55cf53deced2b612c6e1e46061674d0582f292ae
jonathan_baraza <jbaraza46@gmail.com>
2023-05-04 23:05:10 +0300

    expo location feature

--- App.js
+++ App.js
@@ -2,24 +2,42 @@ import React, { useState, useEffect } from "react";
 import { ActivityIndicator, View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
+import * as Location from "expo-location";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== "granted") {
+        setError("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
   }, []);
+
+  if (location) {
+    console.log("location");
+    console.log(location);
+  }
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={"large"} color={"#007acc"} />
+      </View>
+    );
+  }
   return (
     <NavigationContainer>
-      {loading ? (
-        <View style={styles.container}>
-          <ActivityIndicator size={"large"} color={"#007acc"} />
-        </View>
-      ) : (
-        <Tabs />
-      )}
+      <Tabs />
     </NavigationContainer>
   );
 };
